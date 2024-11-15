3f4de445da757fa4846d09f864aefd91dc817d22
Abdullo <147158343+Abdushahidov07@users.noreply.github.com>
2024-11-15 19:07:43 +0500

    I forget this page

--- api/serializers.py
+++ api/serializers.py
@@ -3,17 +3,17 @@ from rest_framework import serializers
 from .models import *
 
 
-class CategorySerializers(serializers.ModelSerializer):
+class MovieSerializers(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = "__all__"
 
-class VakansionSerializers(serializers.ModelSerializer):
+class GenresSerializers(serializers.ModelSerializer):
     class Meta:
         model = Genres
         fields = "__all__"
 
-class ApplicattionSerializers(serializers.ModelSerializer):
+class ReviewerSerializers(serializers.ModelSerializer):
     class Meta:
         model = Reviewer
         fields = "__all__"
